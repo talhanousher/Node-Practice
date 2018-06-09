327b1480fe5086c8b3dd8899e7d954072d1b9e70
talhasudofy <talha@sudofy.com>
2018-06-09 17:26:19 +0500

    JSON output

--- HTTP/app.js
+++ HTTP/app.js
@@ -6,9 +6,16 @@ var server = http.createServer(function (req, res) {
     // var html = fs.readFileSync(__dirname + '/index.html').toString();
     // var message = 'Hello Talha !!!';
     // html = html.replace('{message}', message);
-    fs.createReadStream(__dirname+'/index.html').pipe(res);
+    // fs.createReadStream(__dirname+'/index.html').pipe(res);
 
     // res.end(html);
+
+
+    var obj = {
+        firstName: 'Talha',
+        lastName: 'Nousher',
+    }
+    res.end(JSON.stringify(obj));
 });
 
 server.listen(1300, '127.0.0.1');
