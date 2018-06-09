9a323c9846986c8cee9690321a1f335b40e292fd
talhasudofy <talha@sudofy.com>
2018-06-09 16:02:36 +0500

    Rendering Streams

--- HTTP/app.js
+++ HTTP/app.js
@@ -3,10 +3,12 @@ var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    var html = fs.readFileSync(__dirname + '/index.html').toString();
-    var message = 'Hello Talha !!!';
-    html = html.replace('{message}', message);
-    res.end(html);
+    // var html = fs.readFileSync(__dirname + '/index.html').toString();
+    // var message = 'Hello Talha !!!';
+    // html = html.replace('{message}', message);
+    fs.createReadStream(__dirname+'/index.html').pipe(res);
+
+    // res.end(html);
 });
 
 server.listen(1300, '127.0.0.1');
